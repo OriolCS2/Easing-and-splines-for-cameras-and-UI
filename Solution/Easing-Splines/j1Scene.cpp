502e7a982cbcc5522ee6719c678ce4438eb80e58
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-24 20:36:06 +0200

    each spline has a control

--- Solution/Easing-Splines/j1Scene.cpp
+++ Solution/Easing-Splines/j1Scene.cpp
@@ -66,7 +66,31 @@ bool j1Scene::Update(float dt)
 		App->easing_splines->CreateSpline(&quad.x, quad.x + 800, 3000, TypeSpline::EASE_IN_CIRC);
 	}*/
 
-	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_DOWN) {
+	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_IN_CIRC);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_IN_OUT_BACK);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_4) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_IN_OUT_EXPO);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_5) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_IN_QUAD);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_6) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_OUT_CUBIC);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_7) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_OUT_ELASTIC);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_8) == KEY_DOWN) {
+		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_OUT_QUART);
+	}
+	if (App->input->GetKey(SDL_SCANCODE_9) == KEY_DOWN) {
 		App->easing_splines->CreateSpline(&App->render->camera.x, -App->render->camera.x - 10450, 8000, TypeSpline::EASE_OUT_QUINT);
 	}
 
